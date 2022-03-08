6e68c217e3b337183f2e28289ffc1e0e504f861f
shemanto roy <shemantoroy5@gmail.com>
2022-03-09 01:26:16 +0600

    initial commit

---
+++ learn_flutter/lib/view/screen/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      appBar: AppBar(title: Text('Learn Flutter')),
+      body: Text('sdf'),
+
+    ));
+  }
+}
